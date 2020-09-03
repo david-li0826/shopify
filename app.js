89503facc166c45903394345fa3ff6d2f63f6ea2
zh7li <zh7li@uwaterloo.com>
2020-09-03 16:58:41 -0400

    db settings

--- app.js
+++ app.js
@@ -22,48 +22,18 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // DB
-// [START cloud_sql_mysql_mysql_create_tcp]
-const createTcpPool = () => {
-  // Extract host and port from socket address
-  const dbSocketAddr = process.env.DB_HOST.split(":")
-
-  // Establish a connection to the database
-  return mysql.createConnection({
-    user: process.env.DB_USER, // e.g. 'my-db-user'
-    password: process.env.DB_PASS, // e.g. 'my-db-password'
-    database: process.env.DB_NAME, // e.g. 'my-database'
-    host: dbSocketAddr[0], // e.g. '127.0.0.1'
-    port: dbSocketAddr[1], // e.g. '3306'
-  });
+let config = {
+  user: process.env.SQL_USER,
+  database: process.env.SQL_DATABASE,
+  password: process.env.SQL_PASSWORD
 }
-// [END cloud_sql_mysql_mysql_create_tcp]
 
-// [START cloud_sql_mysql_mysql_create_socket]
-const createUnixSocketPool = () => {
-  const dbSocketPath = process.env.DB_SOCKET_PATH || "/cloudsql"
-
-  // Establish a connection to the database
-  return mysql.createConnection({
-    user: process.env.DB_USER, // e.g. 'my-db-user'
-    password: process.env.DB_PASS, // e.g. 'my-db-password'
-    database: process.env.DB_NAME, // e.g. 'my-database'
-    // If connecting via unix domain socket, specify the path
-    socketPath: `${dbSocketPath}/${process.env.CLOUD_SQL_CONNECTION_NAME}`,
-  });
+if (process.env.INSTANCE_CONNECTION_NAME && process.env.NODE_ENV === 'production') {
+  config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
 }
-// [END cloud_sql_mysql_mysql_create_socket]
 
-const createConnection = () => {
-  if (process.env.DB_HOST) {
-    return createTcpPool();
-  } else {
-    return createUnixSocketPool();
-  }
-
-};
-// [END cloud_sql_mysql_mysql_create]
-
-const db = createConnection();
+let db = mysql.createConnection(config);
+db.connect();
 global.db = db;
 
 app.use('/', indexRouter);
