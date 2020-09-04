8ceec0edc390e31a0acbc7eee4be36d7aebdf6a4
zh7li <zh7li@uwaterloo.com>
2020-09-04 18:56:22 -0400

    settings update

--- app.js
+++ app.js
@@ -26,13 +26,25 @@ app.use(fileUpload());
 
 // DB
 const dbSocketAddr = process.env.SQL_HOST.split(":");
+const dbSocketPath = process.env.DB_SOCKET_PATH || "/cloudsql";
 
-let config = {
-  user: process.env.SQL_USER,
-  database: process.env.SQL_DATABASE,
-  password: process.env.SQL_PASSWORD,
-  host: dbSocketAddr[0],
-  port: dbSocketAddr[1]
+let config;
+
+if (process.env.SQL_HOST) {
+  config = {
+    user: process.env.SQL_USER,
+    database: process.env.SQL_DATABASE,
+    password: process.env.SQL_PASSWORD,
+    host: dbSocketAddr[0],
+    port: dbSocketAddr[1]
+  }
+} else {
+  config = {
+    user: process.env.SQL_USER,
+    database: process.env.SQL_DATABASE,
+    password: process.env.SQL_PASSWORD,
+    socketPath: `${dbSocketPath}/${process.env.INSTANCE_CONNECTION_NAME}`
+  }
 }
 
 let db = mysql.createConnection(config);
